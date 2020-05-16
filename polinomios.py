b52a41766ceeba264786e74f30301db95881f2ba
Rulstar <chistosito80@hotmail.com>
2020-05-16 12:05:10 -0500

    Ya quedó V.S. 1.2

--- polinomios.py
+++ polinomios.py
@@ -3,7 +3,7 @@ p = np.poly1d([])
 tit="raíces racionales".capitalize()
 print(tit.center(50, "="))
 n=int(input('Dame el valor del grado de tu polinomio: '))
-print("\nIntroduce los valores de tus coeficientes de orden descendente.\n\t\t\t\t\t\t\t".center(80, "!"))
+print("\nIntroduce los valores de tus coeficientes de forma descendente.\n\t\t\t\t\t\t\t".center(80, "!"))
 for i in range(0, n+1):
     p[n-i] = input('Coeficiente del término x^['+ str(n-i) +']: ')
 print("Tu polinomio es el siguiente:".center(50, "="))
